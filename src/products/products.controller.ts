839f941ffd32c5a2ba13fe085ad0da6217380a45
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-01-20 10:44:08 +0100

    Add option to edit products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateProductDTO } from './dtos/create-product.dto';
 import { ProductsService } from './products.service';
+import { ParseUUIDPipe } from '@nestjs/common';
+import { UpdateProductDTO } from './dtos/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -13,16 +23,24 @@ export class ProductsController {
   }
 
   @Get('/:id')
-  public getById(@Param('id') id: string) {
+  public getById(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.productsService.getById(id);
   }
   @Delete('/:id')
-  public delete(@Param('id') id: string) {
+  public delete(@Param('id', new ParseUUIDPipe()) id: string) {
     this.productsService.deleteById(id);
     return this.productsService.getAll();
   }
   @Post('/')
   create(@Body() productData: CreateProductDTO) {
     return this.productsService.create(productData);
+  }
+
+  @Put('/:id')
+  updateById(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() productData: UpdateProductDTO,
+  ) {
+    return this.productsService.updateById(id, productData);
   }
 }
